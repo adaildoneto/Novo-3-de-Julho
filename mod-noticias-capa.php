1e297eaafac2cd0c11aa159c3b74b58b299bd715
adaildoneto <adaildo.neto@gmail.com>
2018-12-11 13:35:38 -0500

    update

--- mod-noticias-capa.php
+++ mod-noticias-capa.php
@@ -121,35 +121,66 @@
           <?php if ( dynamic_sidebar('h7-sidebar') ) : else : endif; ?>
         </div>
 
+<div class="row">
+  <div class="col S12 m12 l6">
+    <h5 style="padding-bottom: 5px;">Tudo sobre Política</h5>
+    <hr class="style-one">
+    <?php
 
-          <div class="col S12 m12 l12">
-            <h5 style="padding-bottom: 5px;">Tudo sobre Política</h5>
-            <hr class="style-one">
-            <?php
+      $myargs = array (
+        'pagination'             => false,
+        'cat'							   			=> array(6),
+        'posts_per_page'         => 5,
+        'ignore_sticky_posts'    => true,
 
-              $myargs = array (
-                'pagination'             => false,
-                'cat'							   			=> array(6),
-                'posts_per_page'         => 5,
-                'ignore_sticky_posts'    => true,
+      );
+      // The Query
+      $myquery = new WP_Query( $myargs );
 
-              );
-              // The Query
-              $myquery = new WP_Query( $myargs );
+      // The Loop
+      while ( $myquery->have_posts() ) {
 
-              // The Loop
-              while ( $myquery->have_posts() ) {
-
-                  $myquery->the_post();
-
-
-
-                  get_template_part( 'cardnews', 'hgrande' );
+          $myquery->the_post();
 
 
-                        wp_reset_postdata();
 
-                }
+          get_template_part( 'cardnews', 'hgrande' );
 
-              ?>
-          </div>
+
+                wp_reset_postdata();
+
+        }
+
+      ?>
+  </div>
+  <div class="col S12 m12 l6">
+    <?php
+
+      $myargs = array (
+        'pagination'             => false,
+        'cat'							   			=> array(6),
+        'offset'                  => 5,
+        'posts_per_page'         => 5,
+        'ignore_sticky_posts'    => true,
+
+      );
+      // The Query
+      $myquery = new WP_Query( $myargs );
+
+      // The Loop
+      while ( $myquery->have_posts() ) {
+
+          $myquery->the_post();
+
+
+
+          get_template_part( 'cardnews', 'hgrande' );
+
+
+                wp_reset_postdata();
+
+        }
+
+      ?>
+  </div>
+</div>
