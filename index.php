3aa46e1a2871fd6eeb4e848f4ae8c427f3e7f158
adaildoneto <adaildo.neto@gmail.com>
2018-12-10 23:24:18 -0500

    colunistas menores

--- index.php
+++ index.php
@@ -28,18 +28,20 @@ get_header(); ?>
   </div>
 </div>
 
-<div id="colunas" class="row">
+<div id="colunas" class="row orange lighten-5" style="margin-top: -20px;">
   <div class="col s12 m12 l12">
-    <h5 style="padding-bottom: 5px;">Colunas</h5>
-    <hr class="style-one">
+    <h5 class="red-text text-darken-4" style="padding-bottom: 5px; font-weight: 700;">Colunas</h5>
+    <div class="diviser">
+
+    </div>
   </div>
 
 
   <div class="col s12 m12 l3">
   <div class="card transparent z-depth-0 center">
-    <img class="circle responsive-img z-depth-1" style="width: 150px; height: 150px;" src="https://3dejulhonoticias.com.br/wp-content/uploads/2018/12/47591999_1013389978845318_4001141784347410432_n-e1544499406711.gif">
+    <img class="circle responsive-img z-depth-1" style="width: 100px; height: 100px;" src="https://3dejulhonoticias.com.br/wp-content/uploads/2018/12/47591999_1013389978845318_4001141784347410432_n-e1544499406711.gif">
 
-    <div class=""><a href="https://3dejulhonoticias.com.br/blog-do-crica/"><h4 class="red-text text-darken-4" style="font-size: 1.6rem; font-weight: 800; padding: 5px;">Coluna do Crica</h4></a>
+    <div class=""><a href="https://3dejulhonoticias.com.br/blog-do-crica/"><h4 class="red-text text-darken-4" style="font-size: 1.2rem; font-weight: 800; padding: 5px;">Coluna do Crica</h4></a>
     </div>
 
   </div>
@@ -47,9 +49,9 @@ get_header(); ?>
 
   <div class="col s12 m12 l3">
   <div class="card transparent z-depth-0 center">
-    <img class="circle responsive-img z-depth-1" style="width: 150px; height: 150px;" src="https://3dejulhonoticias.com.br/wp-content/uploads/2015/01/asterio-e1544500027182.png">
+    <img class="circle responsive-img z-depth-1" style="width: 100px; height: 100px;" src="https://3dejulhonoticias.com.br/wp-content/uploads/2015/01/asterio-e1544500027182.png">
 
-    <div class=""><a href="https://3dejulhonoticias.com.br/coluna-do-asterio-moreira/"><h4 class="red-text text-darken-4" style="font-size: 1.6rem; font-weight: 800; padding: 5px;">Coluna do Astério Moreira</h4></a>
+    <div class=""><a href="https://3dejulhonoticias.com.br/coluna-do-asterio-moreira/"><h4 class="red-text text-darken-4" style="font-size: 1.2rem; font-weight: 800; padding: 5px;">Coluna do Astério Moreira</h4></a>
     </div>
 
   </div>
@@ -57,9 +59,9 @@ get_header(); ?>
 
   <div class="col s12 m12 l3">
   <div class="card transparent z-depth-0 center">
-    <img class="circle responsive-img z-depth-1" style="width: 150px; height: 150px;" src="https://3dejulhonoticias.com.br/wp-content/uploads/2015/02/JACKS-e1544500270218.png">
+    <img class="circle responsive-img z-depth-1" style="width: 100px; height: 100px;" src="https://3dejulhonoticias.com.br/wp-content/uploads/2015/02/JACKS-e1544500270218.png">
 
-    <div class=""><a href="https://3dejulhonoticias.com.br/jacks-aroldo/"><h4 class="red-text text-darken-4" style="font-size: 1.6rem; font-weight: 800; padding: 5px;">Coluna do Jacks Aroldo</h4></a>
+    <div class=""><a href="https://3dejulhonoticias.com.br/jacks-aroldo/"><h4 class="red-text text-darken-4" style="font-size: 1.2rem; font-weight: 800; padding: 5px;">Coluna do Jacks Aroldo</h4></a>
     </div>
 
   </div>
@@ -67,9 +69,9 @@ get_header(); ?>
 
   <div class="col s12 m12 l3">
   <div class="card transparent z-depth-0 center">
-    <img class="circle responsive-img z-depth-1" style="width: 150px; height: 150px;" src="https://3dejulhonoticias.com.br/wp-content/uploads/2018/12/48275316_1621205861312922_7401799174519259136_n.jpg">
+    <img class="circle responsive-img z-depth-1" style="width: 100px; height: 100px;" src="https://3dejulhonoticias.com.br/wp-content/uploads/2018/12/48275316_1621205861312922_7401799174519259136_n-e1544500804882.jpg">
 
-    <div class=""><a href="https://3dejulhonoticias.com.br/politica-sem-fronteira/"><h4 class="red-text text-darken-4" style="font-size: 1.6rem; font-weight: 800; padding: 5px;">Coluna do Amaral</h4></a>
+    <div class=""><a href="https://3dejulhonoticias.com.br/politica-sem-fronteira/"><h4 class="red-text text-darken-4" style="font-size: 1.2rem; font-weight: 800; padding: 5px;">Coluna do Amaral</h4></a>
     </div>
 
   </div>
