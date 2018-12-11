848470b254b8fa337b55f0a095ab7b331b7d5697
adaildoneto <adaildo.neto@gmail.com>
2018-12-11 13:49:29 -0500

    novo update

--- cardnews-hgrande.php
+++ cardnews-hgrande.php
@@ -1,7 +1,7 @@
 
   <a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
         <div class="card transparent horizontal z-depth-0">
-          
+
               <div class="card-image">
                 <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'big-quadrado' );?>
                 <img class="responsive-img" src="<?php echo $image[0]; ?>" >
@@ -11,9 +11,7 @@
                   <?php the_title();?>
                   <hr class="style-one">
                   </div>
-                  <div class="news-text blue-grey-text hide-on-med-and-down">
-                  <?php echo excerpt('20'); ?>
-                  </div>
+                  
               </div>
             </div>
 </a>
