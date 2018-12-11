3605f86c8fd0167f913a884e1d3863771052b778
adaildoneto <adaildo.neto@gmail.com>
2018-12-11 13:41:26 -0500

    bug

--- index.php
+++ index.php
@@ -91,6 +91,69 @@ get_header(); ?>
   		<?php get_sidebar();?>
 
 
+        <div class="col S12 m12 l6">
+          <h5 style="padding-bottom: 5px;">Tudo sobre Política</h5>
+          <hr class="style-one">
+          <?php
+
+            $myargs = array (
+              'pagination'             => false,
+              'cat'							   			=> array(6),
+              'posts_per_page'         => 5,
+              'ignore_sticky_posts'    => true,
+
+            );
+            // The Query
+            $myquery = new WP_Query( $myargs );
+
+            // The Loop
+            while ( $myquery->have_posts() ) {
+
+                $myquery->the_post();
+
+
+
+                get_template_part( 'cardnews', 'hgrande' );
+
+
+                      wp_reset_postdata();
+
+              }
+
+            ?>
+        </div>
+        <div class="col S12 m12 l6">
+          <?php
+
+            $myargs = array (
+              'pagination'             => false,
+              'cat'							   			=> array(6),
+              'offset'                  => 5,
+              'posts_per_page'         => 5,
+              'ignore_sticky_posts'    => true,
+
+            );
+            // The Query
+            $myquery = new WP_Query( $myargs );
+
+            // The Loop
+            while ( $myquery->have_posts() ) {
+
+                $myquery->the_post();
+
+
+
+                get_template_part( 'cardnews', 'hgrande' );
+
+
+                      wp_reset_postdata();
+
+              }
+
+            ?>
+        </div>
+
+
 	</div>
 
 
