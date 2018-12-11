10f8e290c502a660f2814fcb0fb24f21e8656923
adaildoneto <adaildo.neto@gmail.com>
2018-12-10 21:33:36 -0500

    colunas

--- pgasterio.php
+++ pgasterio.php
@@ -5,9 +5,6 @@ Template Name: Astério
 ?>
 <?php get_header(); ?>
     <div class="pgcolunas"><img class="responsive-img" src="http://3dejulhonoticias.com.br/wp-content/themes/3dejulho/library/images/asterio-logo.jpg" alt="Coluna do Asterio"></div>
-
-
-
     <div id="content" class="row">
 
     <div class="col s12 m12 l8">
@@ -25,11 +22,11 @@ Template Name: Astério
 
 
 		<div id="sidebar" class="col s12 m12 l4" role="complementary">
-        <div><h4>O Colunista.</h4>
+        <div><h8>O Colunista.</h8>
         		<p>Jornalista, radialista e apresentador de televisão.</p><br /><hr><br />
         </div>
 
-        <h4 style="margin-bottom:10px; border-bottom: dashed 1px #CCCCCC; ">VEJA OUTRAS PUBLICAÇÕES DESTA COLUNA</h4>
+        <h8 style="margin-bottom:10px; border-bottom: dashed 1px #CCCCCC; ">VEJA OUTRAS PUBLICAÇÕES DESTA COLUNA</h8>
 
         <?php query_posts('offset=2&cat=29&showposts=10'); ?>
                              <?php if (have_posts()): while (have_posts()) : the_post();?>
--- pgecrica.php
+++ pgecrica.php
@@ -4,72 +4,40 @@ Template Name: Crica
 */
 ?>
 <?php get_header(); ?>
-    <div class="pgcolunas"><img src="https://3dejulhonoticias.com.br/wp-content/themes/3dejulho/library/images/crica-logo.jpg" width="967px" alt="Blog do Crica"></div>
-    
-<?php query_posts('cat=28&posts_per_page=1'); ?>
-	
-    <div id="content" class="clearfix">
-        
-    <div id="content" class="clearfix">
-        
-        <div id="main" class="col620 clearfix" role="main">
+    <div class="pgcolunas"><img class="responsive" src="https://3dejulhonoticias.com.br/wp-content/themes/3dejulho/library/images/crica-logo.jpg" width="967px" alt="Blog do Crica"></div>
 
-			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'contentoff', 'single' ); ?>
+    <div id="content" class="row">
 
-				<?php attorney_content_nav( 'nav-below' ); ?>
+      <div class="col s12 m12 l8">
+        <?php query_posts('cat=28&posts_per_page=1'); ?>
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() )
-						comments_template( '', true );
-				?>
+          <?php while ( have_posts() ) : the_post(); ?>
 
-			<?php endwhile; // end of the loop. ?>
+          <?php get_template_part( 'content', '' ); ?>
 
-        </div> <!-- end #main -->
 
-		<div id="sidebar" class="widget-area col300" role="complementary">
-        <div><h4>O Colunista.</h4>
+
+        <?php endwhile; // end of the loop. ?>
+
+          </div> <!-- end #main -->
+
+
+		<div id="sidebar" class="col s12 m12 l4" role="complementary">
+        <div><h8>O Colunista.</h8>
         		<p>Luís Carlos Moreira Jorge, formado em Direito, militante há 40 anos no jornalismo político. Foi diretor de Divulgação dos Governos Geraldo Mesquita e Joaquim Macedo, secretário de Comunicação dos governadores Nabor Junior, Iolanda Lima, Romildo Magalhães e dos prefeitos Mauri Sérgio, Flaviano Melo e Isnard Leite. Trabalhou em O RIO BRANCO, O JORNAL, na Difusora Acreana, TV-ACRE, TV-GAZETA e Jornal GAZETA, onde escreve hoje uma coluna política. É um dos nomes mais respeitados do jornalismo acreano.</p><br /><hr><br />
         </div>
+        <h8 style="margin-bottom:10px; border-bottom: dashed 1px #CCCCCC; ">VEJA OUTRAS PUBLICAÇÕES DESTA COLUNA</h8>
 
-<?php if(function_exists( 'wp_bannerize' ))
-	wp_bannerize( 'group=Crica1' ); ?><br />
+        <?php query_posts('offset=1&cat=28&showposts=10'); ?>
+                             <?php if (have_posts()): while (have_posts()) : the_post();?>
 
-		<?php if ( ! dynamic_sidebar( 'Coluna-Crica' ) ) : ?>
+                                <?php get_template_part( 'cardnews', '' ); ?>
+                          <?php endwhile; else:?>
+        		       <?php endif;?>
 
-		<?php endif; // end sidebar widget area ?>
-        </div>
-        
-        
-                    <div id="sidebar" class="widget-area col300" role="complementary">
-
-<h4 style="margin-bottom:10px; border-bottom: dashed 1px #CCCCCC; ">VEJA OUTRAS PUBLICAÇÕES DESTA COLUNA</h4>    
-    
-<?php query_posts('offset=2&cat=28&showposts=10'); ?>       
-                     <?php if (have_posts()): while (have_posts()) : the_post();?>
-               
-<div class="coluna-wdg">
-
-
-<div><h4><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h4></div>
-
-	<div>
-    <strong><?php the_date(); ?></strong>
     </div>
-    <div>
-		<?php the_excerpt(); ?>
-	</div>
-
-</div>
-                  <?php endwhile; else:?>
-		       <?php endif;?>
-
-			</div>
-        
-
+    <?php get_sidebar() ?>           
     </div> <!-- end #content -->
-        
+
 <?php get_footer(); ?>
