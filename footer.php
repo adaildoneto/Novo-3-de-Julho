883bff1fdebdee3d8e86cf17d6a0190f1a733058
adaildoneto <adaildo.neto@gmail.com>
2019-03-02 13:47:09 -0500

    r

--- footer.php
+++ footer.php
@@ -9,18 +9,6 @@
  * @since 2.2.0
  */
 ?>
-
-<?php
-/**
- * The template for displaying the footer.
- *
- * Contains footer content and the closing of the
- * #main div element.
- *
- * @package Odin
- * @since 2.2.0
- */
-?>
 </div>
 </div>
 </div>
