40ba2243bda1acb331935a9472196b9587952210
adaildoneto <adaildo.neto@gmail.com>
2019-06-29 23:04:54 -0500

    novo3dejulho

---
+++ mod-noticias-nova.php
@@ -0,0 +1,58 @@
+
+
+<div class="row">
+  <?php if ( dynamic_sidebar('h1-sidebar') ) : else : endif; ?>
+
+
+</div>
+
+
+<div class="row">
+  <?php if ( dynamic_sidebar('h2-sidebar') ) : else : endif; ?>
+</div>
+
+
+  <div class="col S12 m6 l3">
+  <?php if ( dynamic_sidebar('n1-sidebar') ) : else : endif; ?>
+  </div>
+
+  <div class="col S12 m6 l3">
+<?php if ( dynamic_sidebar('n2-sidebar') ) : else : endif; ?>
+  </div>
+
+  <div class="row">
+    <?php if ( dynamic_sidebar('h3-sidebar') ) : else : endif; ?>
+  </div>
+
+
+    <div class="col S12 m12 l6">
+      <?php if ( dynamic_sidebar('n3-sidebar') ) : else : endif; ?>
+    </div>
+
+    <div class="col S12 m12 l6">
+      <?php if ( dynamic_sidebar('n4-sidebar') ) : else : endif; ?>
+    </div>
+    <div class="row">
+      <?php if ( dynamic_sidebar('h4-sidebar') ) : else : endif; ?>
+    </div>
+
+
+      <div class="col S12 m12 l6">
+      <?php if ( dynamic_sidebar('n5-sidebar') ) : else : endif; ?>
+      </div>
+
+      <div class="col S12 m12 l6">
+    <?php if ( dynamic_sidebar('n6-sidebar') ) : else : endif; ?>
+      </div>
+      <div class="row">
+        <?php if ( dynamic_sidebar('h6-sidebar') ) : else : endif; ?>
+      </div>
+
+
+        <div class="col S12 m12 l6">
+          <?php if ( dynamic_sidebar('n7-sidebar') ) : else : endif; ?>
+        </div>
+
+        <div class="col S12 m12 l6">
+              <?php if ( dynamic_sidebar('n8-sidebar') ) : else : endif; ?>
+             </div>
--- page-nova.php
+++ page-nova.php
@@ -13,23 +13,108 @@
 
 get_header(nova); ?>
 
-	<main id="content" class="<?php echo odin_classes_page_full(); ?>" tabindex="-1" role="main">
 
-			<?php
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
+<div class="row">
+  <?php if ( dynamic_sidebar('destaque-sidebar') ) : else : endif; ?>
+</div>
+<div class="row black" style="padding: 20px;">
+  <div class="col s12 m12 l8">
+      <?php if ( dynamic_sidebar('video-sidebar') ) : else : endif; ?>
+  </div>
 
-					// Include the page content template.
-					get_template_part( 'content', 'page' );
+  <div class="col s12 m12 l4">
+    <?php if ( dynamic_sidebar('listavideo-sidebar') ) : else : endif; ?>
+  </div>
+</div>
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				endwhile;
-			?>
+<div class="row" style="margin-top: -20px;">
+  <div class="col s12 m12 l12">
+    <?php if ( dynamic_sidebar('banner_prefeitura') ) : else : endif; ?>
+  </div>
+</div>
 
-	</main><!-- #main -->
+
+
+	<div class="row">
+
+				<?php include(TEMPLATEPATH.'/mod-noticias-nova.php');?>
+
+
+	</div>
+
+  <div class="row">
+    <?php if ( dynamic_sidebar('h7-sidebar') ) : else : endif; ?>
+  </div>
+
+
+<div class="row">
+  <div class="col s12 m12 l12">
+    <h5 style="padding-bottom: 5px;">Tudo sobre Política</h5>
+    <hr class="style-one">
+    </div>
+  <div class="col S12 m12 l6">
+
+    <?php
+
+      $myargs = array (
+        'pagination'             => false,
+        'cat'							   			=> array(6),
+        'posts_per_page'         => 5,
+        'ignore_sticky_posts'    => true,
+
+      );
+      // The Query
+      $myquery = new WP_Query( $myargs );
+
+      // The Loop
+      while ( $myquery->have_posts() ) {
+
+          $myquery->the_post();
+
+
+
+          get_template_part( 'cardnews', 'hgrande' );
+
+
+                wp_reset_postdata();
+
+        }
+
+      ?>
+  </div>
+  <div class="col S12 m12 l6">
+    <?php
+
+      $myargs = array (
+        'pagination'             => false,
+        'cat'							   			=> array(6),
+        'offset'                  => 5,
+        'posts_per_page'         => 5,
+        'ignore_sticky_posts'    => true,
+
+      );
+      // The Query
+      $myquery = new WP_Query( $myargs );
+
+      // The Loop
+      while ( $myquery->have_posts() ) {
+
+          $myquery->the_post();
+
+
+
+          get_template_part( 'cardnews', 'hgrande' );
+
+
+                wp_reset_postdata();
+
+        }
+
+      ?>
+  </div>
+
+</div>
+
 
 <?php
 get_footer();
