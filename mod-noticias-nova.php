93e50c00672361b0f19b04ed12ee2f523c14f581
adaildoneto <adaildo.neto@gmail.com>
2019-07-03 00:22:17 -0500

    sasdd

--- mod-noticias-nova.php
+++ mod-noticias-nova.php
@@ -127,6 +127,133 @@
   </div>
 
 
+  <div class="row">
+    <?php
+
+      $myargs = array (
+        'pagination'             => false,
+        'cat'							   			=> array(17),
+        'posts_per_page'         => 4,
+        'offset'                  => 0,
+        'ignore_sticky_posts'    => true,
+
+      );
+      // The Query
+      $myquery = new WP_Query( $myargs );
+
+      // The Loop
+      while ( $myquery->have_posts() ) {
+
+          $myquery->the_post();
+
+
+          echo ('<div class="col s12 m6 l3">');
+          get_template_part( 'cardnews', '' );
+          echo ('</div>');
+
+                wp_reset_postdata();
+
+        }
+
+      ?>
+
+  </div>
+  <div class="row">
+    <?php
+
+      $myargs = array (
+        'pagination'             => false,
+        'cat'							   			=> array(17),
+        'posts_per_page'         => 4,
+        'offset'                  => 4,
+        'ignore_sticky_posts'    => true,
+
+      );
+      // The Query
+      $myquery = new WP_Query( $myargs );
+
+      // The Loop
+      while ( $myquery->have_posts() ) {
+
+          $myquery->the_post();
+
+
+          echo ('<div class="col s12 m6 l3">');
+          get_template_part( 'cardnews', '' );
+          echo ('</div>');
+
+                wp_reset_postdata();
+
+        }
+
+      ?>
+
+  </div>
+
+  <div class="row">
+    <?php
+
+      $myargs = array (
+        'pagination'             => false,
+        'cat'							   			=> array(2),
+        'posts_per_page'         => 4,
+        'offset'                  => 0,
+        'ignore_sticky_posts'    => true,
+
+      );
+      // The Query
+      $myquery = new WP_Query( $myargs );
+
+      // The Loop
+      while ( $myquery->have_posts() ) {
+
+          $myquery->the_post();
+
+
+          echo ('<div class="col s12 m6 l3">');
+          get_template_part( 'cardnews', '' );
+          echo ('</div>');
+
+                wp_reset_postdata();
+
+        }
+
+      ?>
+
+  </div>
+  <div class="row">
+    <?php
+
+      $myargs = array (
+        'pagination'             => false,
+        'cat'							   			=> array(2),
+        'posts_per_page'         => 4,
+        'offset'                  => 4,
+        'ignore_sticky_posts'    => true,
+
+      );
+      // The Query
+      $myquery = new WP_Query( $myargs );
+
+      // The Loop
+      while ( $myquery->have_posts() ) {
+
+          $myquery->the_post();
+
+
+          echo ('<div class="col s12 m6 l3">');
+          get_template_part( 'cardnews', '' );
+          echo ('</div>');
+
+                wp_reset_postdata();
+
+        }
+
+      ?>
+
+  </div>
+
+
     <div class="col S12 m6 l3">
       <?php if ( dynamic_sidebar('n3-sidebar') ) : else : endif; ?>
     </div>
