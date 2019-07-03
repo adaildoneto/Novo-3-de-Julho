cd9706494984f5e47596a428d80f175b2b031922
adaildoneto <adaildo.neto@gmail.com>
2019-07-03 01:29:10 -0500

    asas

--- mod-noticias-nova.php
+++ mod-noticias-nova.php
@@ -106,10 +106,10 @@
     ?>
 
 </div>
-<div class="row">
-  <div class="col s12 m12 l12">
+<div class="row center-align">
+
     <?php if ( dynamic_sidebar('banner_prefeitura') ) : else : endif; ?>
-  </div>
+
 </div>
   <div class="row black" style="padding: 20px;">
     <div class="col s12 m12 l8">
@@ -254,7 +254,7 @@
   </div>
 
 
-    <div class="row">
+    <div class="row center-align">
       <?php if ( dynamic_sidebar('h5-sidebar') ) : else : endif; ?>
     </div>
 
@@ -274,15 +274,50 @@
     <?php if ( dynamic_sidebar('n6-sidebar') ) : else : endif; ?>
       </div>
 
-      <div class="row">
+      <div class="row center-align">
         <?php if ( dynamic_sidebar('h6-sidebar') ) : else : endif; ?>
       </div>
 
 
-        <div class="col S12 m12 l6">
+        <div class="col S12 m6 l3">
           <?php if ( dynamic_sidebar('n7-sidebar') ) : else : endif; ?>
         </div>
+        <div class="col S12 m6 l3">
+        <?php if (function_exists ('adinserter')) echo adinserter (1); ?>
+        </div>
 
-        <div class="col S12 m12 l6">
+        <div class="col S12 m6 l3">
               <?php if ( dynamic_sidebar('n8-sidebar') ) : else : endif; ?>
+             </div>
+
+             <div class="col S12 m6 l3">
+               <?php
+
+                 $myargs = array (
+                   'pagination'             => false,
+                   'cat'							   			=> array(15),
+                   'posts_per_page'         => 6,
+                   'offset'                  => 4,
+                   'ignore_sticky_posts'    => true,
+
+                 );
+                 // The Query
+                 $myquery = new WP_Query( $myargs );
+
+                 // The Loop
+                 while ( $myquery->have_posts() ) {
+
+                     $myquery->the_post();
+
+
+
+                     get_template_part( 'cardnews', 'horizontal' );
+
+
+                           wp_reset_postdata();
+
+                   }
+
+                 ?>
+
              </div>
